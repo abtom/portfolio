7c6792578d7b0d4343d6b1231e051635b1c2c3dc
Aaron Tom <atom@us.ibm.com>
2016-09-07 11:27:00 -0700

    Fade effect and label

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,3 +1,19 @@
 $(document).ready(function () {
     $('div.fadeIn').fadeIn(1000).removeClass('fadeIn');
+
+    $('#sportify').hover(function(){
+    	$(this).stop().animate({opacity: .4}, 200);
+    $('.text_sportify').removeClass('hide_sportify');
+    	}, function(){
+    		$(this).stop().animate({opacity: 1}, 500);
+    		$('.text_sportify').addClass('hide_sportify');
+    });
+
+    $('#weatherware').hover(function(){
+    	$(this).stop().animate({opacity: .4}, 200);
+    $('.text_weatherware').removeClass('hide_weatherware');
+    	}, function(){
+    		$(this).stop().animate({opacity: 1}, 500);
+    		$('.text_weatherware').addClass('hide_weatherware');
+    });
 });
